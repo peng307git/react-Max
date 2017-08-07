1b8dfc7ec164a3c7d04e8b8e42a0a12d652bb2e3
peng307git <2324964369@qq.com>
2017-08-07 14:26:44 +0800

    fix

--- demo_1/webpack.config.js
+++ demo_1/webpack.config.js
@@ -25,6 +25,30 @@ module.exports = {
             {
                 test : /\.css$/,
                 use :  ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use : ['url-loader?limit=8192']
+            },
+            {
+                test : /\.(mp4|ogg|svg)$/,
+                use : ['file-loader']
+            },
+            {
+                test : /\.(woff|woff2)(\?v=\d+\.\d+)?$/,
+                use : ['url-loader?limit=10000&mimetype=application/font-woff']
+            },
+            {
+                test : /\.ttf(\?v=\d+\.\d+)?$/,
+                use : ['url-loader?limit=10000&mimetype=application/octet-stream']
+            },
+            {
+                test : /\.eot(\?v=\d+\.\d+)?$/,
+                use : ['file-loader']
+            },
+            {
+                test : /\.svg(\?v=\d+\.\d+)?$/,
+                use : ['url-loader?limit=10000&mimetype=image/svg+xml']
             }
         ]
     },
