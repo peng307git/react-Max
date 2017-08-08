7a72f95ab24b2255b337b84d5088a7f1483f1045
peng307git <2324964369@qq.com>
2017-08-08 10:10:12 +0800

    fix

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+require('../Semantic-UI/dist/semantic.css')
+require('./common/style/main.css')
 
+class Nav extends React.Component{
+    render(){
+        return(
+            <div className="ui menu">
+                <div className="header item">left</div>
+                <div className="item">home</div>
+                <div className="item">right</div>
+            </div>
+        )
+    }
+}
 
+class Img extends React.Component {
+    render() {
+        return (
+            <img className="img" src={require('./common/img/index-new01-CN.png')} />
+        );
+    }
+}
+       
 ReactDOM.render(
-    <div>hello react </div>,
+    <div className="ui container">
+        <div className="ui dividing"></div>
+        <Nav/>
+        <Img/>
+        <Nav/>
+    </div>,
     document.getElementById("root")
 );
 
