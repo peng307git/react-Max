564d38a26cd65255a111df9e17ac81e26646c24a
peng307git <2324964369@qq.com>
2017-08-15 10:30:05 +0800

    news auucount

--- todoList/src/app.js
+++ todoList/src/app.js
@@ -82,7 +82,7 @@ export default class App extends React.Component {
                         {items}   
                     </ul> 
                 </section> 
-                <Footer {tianjia}/> 
+                <Footer clearcom = {this.clearcom}/> 
                     
             </div>
         );
